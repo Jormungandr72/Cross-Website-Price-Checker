2620035edcfa4e91afa33c864e6b476c534b2fd1
Justin Clark <jc271299@my.stchas.edu>
2025-02-19 02:54:41 -0600

    Menu navigation, icon, requirements.txt added, react-dom npm package added

--- django_project/src/frontend/src/App.js
+++ django_project/src/frontend/src/App.js
@@ -1,3 +1,12 @@
+// import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Layout from "./pages/layout";
+import Home from "./pages/home";
+import Trends from "./pages/trends";
+import Prices from "./pages/prices";
+import NoPage from "./pages/nopage";
+
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
@@ -9,21 +18,20 @@ function App() {
   useEffect(() => {
     axios.get("http://127.0.0.1:8000/api/test/")
       .then(response => setMessage(response.data.message))
-      .catch(error => setMessage("Error fetching data"));
+      .catch(error => setMessage("Error fetching data at \"http://127.0.0.1:8000/api/test/\""));
   }, []);
 
   return (
-    <div class="navbar">
-      <ul>
-        <li><a>Home</a></li>
-        <li><a>Trends</a></li>
-        <li><a>My Prices</a></li>
-        <li style={{ float: 'right' }}><a>☰</a></li>
-      </ul>
-
-      <p>{message}</p>
-      <h1>This is our mission statement!</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="trends" element={<Trends />} />
+          <Route path="prices" element={<Prices />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
