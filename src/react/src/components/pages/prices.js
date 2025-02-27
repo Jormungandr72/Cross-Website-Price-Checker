d79f5365be5d78a4659f895bb21805188e4f97fb
Justin Clark <jc271299@my.stchas.edu>
2025-02-26 18:52:05 -0600

    supabase initial setup, preliminary login page setup, quickstart-guide.md added, button component added

---
+++ src/react/src/components/pages/prices.js
@@ -0,0 +1,49 @@
+import { useState } from 'react'; // react state hook
+
+import Button from '../button';
+
+import "../../styles/prices.css"
+
+const Prices = () => {
+    const [buttons, setButtons] = useState([]);
+
+    const handleClick = () => {
+        
+        // button list, state updates when length changes
+        const newButtons = [
+            `Button ${buttons.length + 1}`,
+            `Button ${buttons.length + 2}`,
+            `Button ${buttons.length + 3}`,
+        ];
+        setButtons(newButtons); // update buttons var to reflect state change
+        
+        // FIXME: push 3 buttons to end of array to change state and continuously update
+        // updates only up to length = 6
+        buttons.push(`Button ${buttons.length + 1}`);
+        buttons.push(`Button ${buttons.length + 1}`);
+        buttons.push(`Button ${buttons.length + 1}`);
+    };
+
+    return (
+        <div>
+            {/* searchBox div contains all search related components:
+                searchbar input box, search confirm button, related buttons created from query */}
+            <div class="searchBox">
+                <input placeholder="search products"></input>
+                <Button onClick={handleClick}>Button as link</Button>
+
+                {buttons.length > 0 && (
+                    <div>
+                        {buttons.map((label, index) => (
+                        <Button key={index} variant="outline">
+                            {label}
+                        </Button>
+                        ))}
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default Prices;
---
+++ src/supabase/supabase_client.py
@@ -0,0 +1,27 @@
+"""
+-------------------------------------------------------------------------------
+Program:    supabase.py
+Author:     Steven Longhofer
+Date:       02/25/2025
+Language:   python
+Purpose:    initialze a supabase client to communicate with
+-------------------------------------------------------------------------------
+Change Log:
+Who  When           What
+SL  02.25.2025     Created initialization code
+-------------------------------------------------------------------------------
+"""
+
+import os
+from supabase import create_client, Client
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Retrieve Supabase URL and key from environment variables
+supabase_url = os.getenv("SUPABASE_URL")
+supabase_key = os.getenv("SUPABASE_KEY")
+
+# Initialize Supabase client
+supabase: Client = create_client(supabase_url, supabase_key)
