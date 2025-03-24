0699bf88184b626f7867682c8c7b2c0a4514e70b
Jormungandr72 <patrickjmcgranahan@gmail.com>
2025-03-24 11:59:13 -0500

    Created python classes to access Nexar API

---
+++ src/backend/nexar/DataCollector.py
@@ -0,0 +1,27 @@
+"""
+-------------------------------------------------------------------------------
+Program:    DataCollector.py
+Author:     Patrick J. McGranahan
+Date:       03.24.2025
+Language:   python
+Purpose:    The purpose of this code is to collect data from the Nexar API for 
+            use in Price Scout.
+-------------------------------------------------------------------------------
+Change Log:
+Who  When           What
+PJM  03.24.2025     Created NexarClient.py, which may create redundant code.
+                    Keeping DataCollector.py in case it does not.
+PJM  03.24.2025     Created DataCollector.py and added default constructor.
+-------------------------------------------------------------------------------
+"""
+class DataCollector:
+    # Data Fields
+
+    # Constructor
+    def __init__(self):
+        """
+        Default Constructor.
+        """
+        pass
+
+    # Methods
---
+++ src/backend/nexar/NexarClient.py
@@ -0,0 +1,25 @@
+"""
+-------------------------------------------------------------------------------
+Program:    NexarClient.py
+Author:     Patrick J. McGranahan
+Date:       03.24.2025
+Language:   python
+Purpose:    The purpose of this code is to collect data from the Nexar API for 
+            use in Price Scout.
+-------------------------------------------------------------------------------
+Change Log:
+Who  When           What
+PJM  03.24.2025     Created NexarClient.py and added default constructor.
+-------------------------------------------------------------------------------
+"""
+class NexarClient:
+    # Data Fields
+
+    # Constructor
+    def __init__(self):
+        """
+        Default Constructor.
+        """
+        pass
+
+    # Methods
