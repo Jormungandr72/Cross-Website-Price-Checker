94811fcb1d85ebf492a3bb467c3d9ff85f274732
Jormungandr72 <patrickjmcgranahan@gmail.com>
2025-04-03 11:45:19 -0500

    Changes to SupabaseClient.py

--- src/supabase/SupabaseClient.py
+++ src/supabase/SupabaseClient.py
@@ -1,28 +1,35 @@
+import os
+from supabase import create_client, Client
+from dotenv import load_dotenv
+
 """
 -------------------------------------------------------------------------------
 Program:    SupabaseClient.py
 Author:     Steven Longhofer
 Date:       02/25/2025
 Language:   python
-Purpose:    initialze a supabase client to communicate with
+Purpose:    Initialzes a supabase client to communicate with.
 -------------------------------------------------------------------------------
 Change Log:
 Who  When          What
+PJM 04.03.2025     Changed documentation structure and clarified the purpose of
+                   this class.
+PJM 04.03.2025     Refactored current initialization code into a default
+                   constructor.
+PJM 04.03.2025     Began restructuring file, made class SupabaseClient.
 PJM 04.03.2025     Renamed supabase_client.py to SupabaseClient.py.
 SL  02.25.2025     Created initialization code
 -------------------------------------------------------------------------------
 """
+class SupabaseClient:
 
-import os
-from supabase import create_client, Client
-from dotenv import load_dotenv
+    def __init__(self):
+        # Load environment variables from .env file
+        load_dotenv()
+        
+        # Retrieve Supabase URL and key from environment variables
+        self.supabase_url = os.getenv("SUPABASE_URL")
+        self.supabase_key = os.getenv("SUPABASE_KEY")
 
-# Load environment variables from .env file
-load_dotenv()
-
-# Retrieve Supabase URL and key from environment variables
-supabase_url = os.getenv("SUPABASE_URL")
-supabase_key = os.getenv("SUPABASE_KEY")
-
-# Initialize Supabase client
-supabase: Client = create_client(supabase_url, supabase_key)
+        # Initialize Supabase client
+        self.supabase_client = create_client(self.supabase_url, self.supabase_key)
