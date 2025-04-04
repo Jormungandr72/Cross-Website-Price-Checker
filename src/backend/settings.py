52b91129c61a311d1aed9b95e29e62b1cb611a72
Justin Clark <jc271299@my.stchas.edu>
2025-04-04 05:18:00 -0500

    removed sr supabase_key

--- src/backend/settings.py
+++ src/backend/settings.py
@@ -39,7 +39,6 @@ load_dotenv(os.path.join(BASE_DIR, '../.env'))
 # Access these in other files with django.conf.settings
 SUPABASE_URL = os.getenv('SUPABASE_URL')
 SUPABASE_KEY = os.getenv('SUPABASE_KEY')
-SUPABASE_KEY_SR = os.getenv('SUPABASE_KEY_SR')
 
 # Static files settings
 STATIC_URL = '/static/'
