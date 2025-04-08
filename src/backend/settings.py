2f1a8b363ef8b92209ae5ed6b011b170594f153f
Justin Clark <jc271299@my.stchas.edu>
2025-04-07 21:08:58 -0500

    load_dotenv removed escape

--- src/backend/settings.py
+++ src/backend/settings.py
@@ -27,7 +27,7 @@ from dotenv import load_dotenv
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Load environment variables from env file at src
-load_dotenv(os.path.join(BASE_DIR, 'shared-config\.env'))
+load_dotenv(os.path.join(BASE_DIR, 'shared-config', '.env'))
 
 # Access these in other files with django.conf.settings
 SUPABASE_URL = os.getenv('SUPABASE_URL')
