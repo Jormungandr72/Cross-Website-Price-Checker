7b83958df4cfd552d9c4055162d701433b91bba2
Justin Clark <jc271299@my.stchas.edu>
2025-04-12 18:12:28 -0500

    responsive mobile navbar

--- src/react/src/components/pages/layout.jsx
+++ src/react/src/components/pages/layout.jsx
@@ -14,27 +14,18 @@ JC  03.07.2025     set layout for the application pages
 
 import { Outlet, Link } from "react-router-dom";
 
-// import price from "../../price.png"
-
 const Layout = () => {
     return (
         <>
             <nav>
                 <ul>
-                    {/* <li>
-                        <img src={price} alt="Price Logo" id="icon" />
-                    </li> */}
                     <li>
                         <Link to="/">Home</Link>
                     </li>
-
-                    <li><Link>/</Link></li>
                     
                     <li>
                         <Link to="/prices">Prices</Link>
                     </li>
-                    
-                    <li><Link>/</Link></li>
                     
                     <li>
                         <Link to="/login">Login</Link>
