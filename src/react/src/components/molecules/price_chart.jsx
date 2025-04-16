41a6fac54c3dee236299c9e202e87e91ff06f433
Justin Clark <jc271299@my.stchas.edu>
2025-04-16 02:35:27 -0500

    changed dataset

--- src/react/src/components/molecules/price_chart.jsx
+++ src/react/src/components/molecules/price_chart.jsx
@@ -2,17 +2,14 @@ import React from "react";
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from "recharts";
 
 const data = [
-    { time: '2025-04-01', StoreA: 100, StoreB: 120 },
-    { time: '2025-04-02', StoreA: 110, StoreB: 130 },
-    { time: '2025-04-03', StoreA: 120, StoreB: 140 },
-    { time: '2025-04-04', StoreA: 130, StoreB: 150 },
-    { time: '2025-04-05', StoreA: 140, StoreB: 160 },
-    { time: '2025-04-06', StoreA: 150, StoreB: 170 },
-    { time: '2025-04-07', StoreA: 160, StoreB: 180 },
-    { time: '2025-04-08', StoreA: 170, StoreB: 190 },
-    { time: '2025-04-09', StoreA: 180, StoreB: 200 },
-    { time: '2025-04-10', StoreA: 190, StoreB: 210 },
-];
+    { day: 'Monday', sales: 120, revenue: 500 },
+    { day: 'Tuesday', sales: 98, revenue: 430 },
+    { day: 'Wednesday', sales: 86, revenue: 400 },
+    { day: 'Thursday', sales: 99, revenue: 450 },
+    { day: 'Friday', sales: 85, revenue: 420 },
+    { day: 'Saturday', sales: 105, revenue: 480 },
+    { day: 'Sunday', sales: 110, revenue: 490 },
+]
 
 const PriceChart = () => {
     return (
@@ -22,13 +19,48 @@ const PriceChart = () => {
                     data={data}
                     margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
                 >
-                    <CartesianGrid stroke="#666" strokeDasharray="3 3" />
-                    <XAxis dataKey="time" domain={['auto', 'auto']} stroke="#fff"/>
-                    <YAxis domain={['auto', 'auto']} stroke="#fff"/>
-                    <Tooltip contentStyle={{ backgroundColor: '#222', borderColor: '#555', color: '#fff' }}/>
+                    <CartesianGrid 
+                        stroke="#666" 
+                        strokeDasharray="3 3" 
+                    />
+                    <XAxis
+                        dataKey="time"
+                        domain={['auto', 'auto']}
+                        stroke="#fff"
+                        label={
+                            {
+                                value: 'Time',
+                                position: 'insideMiddle',
+                                offset: 0,
+                                fill: '#fff'
+                            }
+                        }
+                    />
+                    <YAxis 
+                        domain={['auto', 'auto']} 
+                        stroke="#fff"
+                        label={
+                            {
+                                value: 'Price',
+                                angle: -90,
+                                position: 'insideLeft',
+                                offset: 0,
+                                fill: '#fff'
+                            }
+                        }
+                    />
+                    <Tooltip 
+                        contentStyle={
+                            {
+                                backgroundColor: '#222',
+                                borderColor: '#555',
+                                color: '#fff'
+                            }
+                        }
+                    />
                     <Legend />
-                    <Line type="monotone" dataKey="StoreA" stroke="#00d8ff" activeDot={{ r: 8 }} />
-                    <Line type="monotone" dataKey="StoreB" stroke="#82ca9d" />
+                    <Line type="monotone" dataKey="sales" stroke="#00d8ff" activeDot={{ r: 8 }} />
+                    <Line type="monotone" dataKey="revenue" stroke="#82ca9d" />
                 </LineChart>
             </ResponsiveContainer>
         </div>
